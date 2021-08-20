e27f9e536c92189279e2fbf45e6d864004c02a09
s-elshot <74968337+s-elshot@users.noreply.github.com>
2021-08-20 18:53:56 +0200

    Integrated counterbutton and reset button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [minutes, setMinutes] = useState(2);
+  const [seconds, setSeconds] = useState(30);
+
+  function Reset(){
+    console.log("Clicked")
+    setMinutes(0);
+    setSeconds(30);
+  }
+
+  function Add30() {
+    if (seconds === 0) {
+      setSeconds(seconds + 30);
+    }
+    if(seconds ===30){
+      setSeconds(0);
+      setMinutes(minutes+1);
+    }
+
+
+
+
+    console.log(seconds)
+    console.log(minutes)
+    }
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Container">
+      <h2 className="Title"> T I M E</h2>
+      <h1 className="TimeData">{minutes}:{seconds}</h1>
+      <button className="Button" onClick={Reset}>r e s e t</button>
+      <button className="Button" onClick={Add30}>a d d 3 0</button>
+      {/*<button className="Button" onClick={Add30}>B E A S T M O D E</button>*/}
+    </div>
+
     </div>
   );
 }
